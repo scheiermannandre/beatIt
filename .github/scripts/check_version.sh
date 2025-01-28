aed0472af8261659d4705c58c58c677dedcdd15d
André Scheiermann <scheiermann.andre@gmail.com>
2025-01-28 09:54:32 +0100

    adjust check version script so that it leaves a clean git state

--- .github/scripts/check_version.sh
+++ .github/scripts/check_version.sh
@@ -3,10 +3,15 @@
 # Get the current version from pubspec.yaml
 CURRENT_VERSION=$(grep "version:" pubspec.yaml | cut -d"+" -f2)
 
+# Create a temporary copy of pubspec.yaml
+cp pubspec.yaml pubspec.yaml.tmp
+
 # Get the previous commit's version
 git checkout HEAD^ pubspec.yaml
 PREVIOUS_VERSION=$(grep "version:" pubspec.yaml | cut -d"+" -f2)
-git checkout HEAD pubspec.yaml
+
+# Restore the current pubspec.yaml
+mv pubspec.yaml.tmp pubspec.yaml
 
 # Compare versions
 if [ "$CURRENT_VERSION" -le "$PREVIOUS_VERSION" ]; then
